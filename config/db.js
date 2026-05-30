75db188b83366d0f8d3107345cf945ee29e4e85d
nusala <nusretala06@gmail.com>
2026-05-30 18:11:01 +0300

    db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'sensizOlmaz06',
+    host: '----',
+    user: '----',
+    password: '-----',
     database: 'OptikOtomasyon',
     waitForConnections: true,
     connectionLimit: 10,
